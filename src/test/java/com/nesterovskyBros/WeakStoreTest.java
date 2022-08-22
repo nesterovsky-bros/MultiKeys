875ad29dd7ba5111363ae71315b3e19a7dcc4df2
Vladimir Nesterovsky <vladimir@nesterovsky-bros.com>
2022-08-22 13:28:17 +0300

    Add comment to unit test.

--- src/test/java/com/nesterovskyBros/WeakStoreTest.java
+++ src/test/java/com/nesterovskyBros/WeakStoreTest.java
@@ -8,12 +8,12 @@ import org.junit.jupiter.api.Test;
 
 class WeakStoreTest
 {
-
   @Test
   void test()
   {
     String[] log = { null };
 
+    // For unit test we override release() to capture the call.
     WeakStore<Object> store = new WeakStore<Object>()
     {
       @Override
@@ -23,46 +23,49 @@ class WeakStoreTest
       }
     };
     
+    // Create keys
     Object key1 = new Object();
     Object key2 = new Object();
+    
+    // and data.
     Object data = new Object();
     
+    // Set data by (key1, key2).
     store.set(data, key1, key2);
     
+    // Verify it's in the store.
     assertTrue(store.get(key1, key2) == data);
     data = null;
     
     gc();
     
+    // Verify data is in the store even after we don't have its firm reference.
     assertNotNull(store.get(key1, key2));
     
+    // Let key2 to go.
     key2 = null;
     
     gc();
     
+    // Some operation over store. It can be any.
     store.poll();
     
-    gc();
-    
+    // Verify that data is released, so entry for (key1, key2) is not there.
     assertNotNull(log[0]);
 
     key2 = new Object();
-    
-    assertTrue(store.get(key1, key2) == null);
+
+    // There is no data by different keys.
+    assertNull(store.get(key1, key2));
   }
   
   private static void gc() 
   {
-    for(int i = 0; i < 2; ++i)
+    WeakReference<Object> ref = new WeakReference<>(new Object());
+  
+    while(ref.get() != null) 
     {
-      WeakReference<Object> ref = new WeakReference<>(new Object());
-    
-      while(ref.get() != null) 
-      {
-        System.gc();
-      }
-      
-      System.runFinalization();
+      System.gc();
     }
   }
 }
